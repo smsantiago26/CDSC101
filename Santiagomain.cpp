81b3ffa86ba96466e4e07c5f93706043f2d6e1d8
smsantiago26 <smsantiago@gbox.adnu.edu.ph>
2025-08-08 14:11:28 +0800

    Create Santiagomain.cpp
    Added main code about simple calculator.

---
+++ Santiagomain.cpp
@@ -0,0 +1,24 @@
+#include <iostream> //input output world
+using namespace std;// for cout and cin
+
+int main()
+{
+
+    int num1, num2, sum, difference, product, quotient, modulo; // declare variables
+    cout << "Enter a number: ";
+    cin >> num1; // storing first user input number into variable
+    cout << "Enter another number: ";
+    cin >> num2; // storing second user input number into another variable
+    sum = num1 + num2; // adding values of variables
+    difference = num1 - num2;
+    product = num1 * num2;
+    quotient = num1 / num2;
+    modulo = num1 % num2;
+    cout << "Your sum is: " << sum << endl; // outputting value of variable sum
+    cout << "Your difference is: " << difference << endl;
+    cout << "Your product is: " << product << endl;
+    cout << "Your quotient is: " << quotient << endl;
+    cout << "Your remainder is: " << modulo << endl;
+    
+    return 0;
+}
